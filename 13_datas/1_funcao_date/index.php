e2dde6912e1937842bc33788f6bf45fe5efe2700
rodrigopepato <rodrigo.pepato@gmail.com>
2024-03-14 00:52:03 -0300

    função date

---
+++ 13_datas/1_funcao_date/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$data1 = date('d/m/y');
+
+echo $data1 . "<br>";
+
+$data2 = date('d, M - Y');
+
+echo $data2 . "<br>";
+
+$data3 = date('d/m/Y');
+
+echo $data3 . "<br>";
+
+$data4 = date('l, M - Y');
+
+echo $data4 . "<br>";
