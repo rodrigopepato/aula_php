9e3144202651cc03855e9e671445cc111e6b57f1
rodrigopepato <rodrigo.pepato@gmail.com>
2024-03-15 18:05:55 -0300

    variavel SERVER

---
+++ 14_PHP_E_WEB/1_server/index.php
@@ -0,0 +1,18 @@
+<?php
+
+print_r($_SERVER);
+
+echo "<br>";
+echo "<br>";
+
+echo $_SERVER['SERVER_SOFTWARE'];
+echo "<br>";
+
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+
+echo $_SERVER['SERVER_PROTOCOL'];
+echo "<br>";
+
+echo $_SERVER['SERVER_PORT'];
+echo "<br>";
