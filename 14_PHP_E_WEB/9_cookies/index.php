81037d708f7fdbdde2729e06af7d8e98a9c01a73
rodrigopepato <rodrigo.pepato@gmail.com>
2024-03-19 14:10:22 -0300

    Cookies

---
+++ 14_PHP_E_WEB/9_cookies/index.php
@@ -0,0 +1,21 @@
+<?php
+
+  setcookie('nome', 'Rodrigo', time() + 3600);
+
+  $nome = isset($_COOKIE['nome']) ? $_COOKIE['nome'] : "";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1> Olá Mundo! </h1>
+  <?php if($nome != ""): ?>
+    <p>Seja bem-vindo <?= $nome ?> </p>
+  <?php endif; ?>
+</body>
+</html>
---
+++ 14_PHP_E_WEB/9_cookies/pagina.php
@@ -0,0 +1,19 @@
+<?php
+
+  $nome = isset($_COOKIE['nome']) ? $_COOKIE['nome'] : "";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1> Página </h1>
+  <?php if($nome != ""): ?>
+    <p>Seja bem-vindo <?= $nome ?> </p>
+  <?php endif; ?>
+</body>
+</html>
