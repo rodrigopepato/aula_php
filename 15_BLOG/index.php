a0fde3febac9b6535bf07e6890bb14a43b6c5799
rodrigopepato <rodrigo.pepato@gmail.com>
2024-03-20 16:13:58 -0300

    Setup do Projeto BlogCodar

---
+++ 15_BLOG/index.php
@@ -0,0 +1,20 @@
+<?php
+  include_once("helpers/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Blog Codar</title>
+  <!-- ESTILOS DO PROJETO -->
+  <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
+  <!-- GOOGLE FONTS -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+</head>
+<body>
+  <h1>Olá Mundo!</h1>
+</body>
+</html>
