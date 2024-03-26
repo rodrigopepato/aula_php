7f27f2a9de883a3fd2bdb13ebce2271be5f41880
rodrigopepato <rodrigo.pepato@gmail.com>
2024-03-25 23:40:29 -0300

    Executando query MySqli

---
+++ 16_PHP_MYSQL/3_execultar_query/index.php
@@ -0,0 +1,16 @@
+<?php
+
+  $host = "localhost";
+  $user = "root";
+  $pass = "";
+  $db = "cursophp";
+
+  $conn = new mysqli($host, $user, $pass, $db);
+
+  $sql = "SELECT * FROM itens";
+
+  $result = $conn->query($sql);
+
+  print_r($result);
+  
+  $conn->close();
