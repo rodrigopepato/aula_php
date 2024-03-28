d22eccadebd0670f22de6cca66a841c0b5134037
rodrigopepato <rodrigo.pepato@gmail.com>
2024-03-28 01:46:16 -0300

    Criando conexão com o banco de dados

---
+++ 17_AGENDA/config/connection.php
@@ -0,0 +1,23 @@
+<?php
+
+$host = "localhost";
+$dbname = "agenda";
+$user = "root";
+$pass = "";
+
+  try 
+  {    
+
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);  
+    
+    // ATIVAR O MEDO DE ERROS
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+  } 
+  catch (PDOException $e) 
+  { // Erro na conexão]
+
+    $error = $e->getMessage();
+    echo "Erro: $error"; 
+
+  }
